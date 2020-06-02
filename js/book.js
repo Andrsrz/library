42a9b115e7db628d8ed35075a60d6fad6e89f9e5
Andres Ruiz <andrsruiz@gmail.com>
2020-06-02 10:24:31 -0500

    Add Book Class

---
+++ js/book.js
@@ -0,0 +1,14 @@
+class Book {
+	/* Default properties values */
+	title = "";
+	author = "";
+	pages = 0;
+	read = false;
+
+	constructor(title, author, pages, read){
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
+}
