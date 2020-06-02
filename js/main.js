b572639b42096aeee776717c57d232ace29cfeae
Andres Ruiz <andrsruiz@gmail.com>
2020-06-02 11:26:37 -0500

    Add New Book button logic

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ var library = new Library();
 
 function loadAddBookForm(){
 	console.log("Works");
+	/* Show the form */
+	let form = document.getElementById("new-book-form-container");
+	if(form.style.display == "flex"){
+		form.style.display = "none";
+	}else{
+		form.style.display = "flex";
+	}
 }
 
 function pageLoaded(){
