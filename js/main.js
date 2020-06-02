ab2cf1988a3a38175a8fa8108df661f3fe7c6e55
Andres Ruiz <andrsruiz@gmail.com>
2020-06-02 13:02:21 -0500

    Add Local Storage Test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,46 @@
 /* Global Library Object */
 var library = new Library();
 
+function pageLoaded(){
+	let form = document.getElementById("form");
+	form.addEventListener("submit", addBook, false);
+	/* render library */
+	let defaultBook = new Book("The Hobbit", "J.R.R. Tolkien", 285, true);
+	library.books.push(defaultBook);
+	renderLibrary();
+	if(storageAvailable('localStorage')){
+		alert("Your browser suppot local storage");	
+	}else {
+		alert("WARNING! Your browser doesn't support local storage. All your book \
+			will be deleted when you close this page.");
+	}
+}
+
+function storageAvailable(type) {
+	var storage;
+	try {
+		storage = window[type];
+		var x = '__storage_test__';
+		storage.setItem(x, x);
+		storage.removeItem(x);
+		return true;
+	}
+	catch(e) {
+		return e instanceof DOMException && (
+			// everything except Firefox
+			e.code === 22 ||
+			// Firefox
+			e.code === 1014 ||
+			// test name field too, because code might not be present
+			// everything except Firefox
+			e.name === 'QuotaExceededError' ||
+			// Firefox
+			e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+			// acknowledge QuotaExceededError only if there's something already stored
+			(storage && storage.length !== 0);
+	}
+}
+
 function loadAddBookForm(){
 	/* Show the form */
 	let form = document.getElementById("new-book-form-container");
@@ -31,13 +71,4 @@ function renderLibrary(){
 		book.appendChild(bookInfo);
 		libraryContainer.appendChild(book);
 	}
-}
-
-function pageLoaded(){
-	let form = document.getElementById("form");
-	form.addEventListener("submit", addBook, false);
-	/* render library */
-	let defaultBook = new Book("The Hobbit", "J.R.R. Tolkien", 285, true);
-	library.books.push(defaultBook);
-	renderLibrary();
 }
